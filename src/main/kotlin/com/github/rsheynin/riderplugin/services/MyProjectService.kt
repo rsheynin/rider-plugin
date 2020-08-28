63b775b8eabea4ca248bad7ab8694578d36c3911
GitHub Action <action@github.com>
2020-08-28 20:28:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/rsheynin/riderplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rsheynin/riderplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rsheynin.riderplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rsheynin.riderplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
